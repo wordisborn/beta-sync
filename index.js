cb6c5f793fb0b2c18dcfd4e52b9c8e5a76e76448
Hilton Campbell <hiltonc@ldsmobileapps.com>
2015-05-29 22:24:24 -0600

    Support GET to satisfy the MailChimp webhook URL validator.

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.post('/webhook', function(request, response) {
+var webhook = function(request, response) {
   if (request.query.key == process.env.API_KEY) {
     response.send('OK\n');
   } else {
     response.status(403).end();
   }
-});
+};
+app.get('/webhook', webhook);
+app.post('/webhook', webhook);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
